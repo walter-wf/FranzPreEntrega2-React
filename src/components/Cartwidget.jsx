e140f35bbb9e4a1e6f660338dcf54440c27651c0
walter-wf <walter.franz@gmail.com>
2024-10-24 13:48:22 -0300

    local repo to remote transfer

---
+++ src/components/Cartwidget.jsx
@@ -0,0 +1,9 @@
+import { CartFill } from 'react-bootstrap-icons';
+
+export const Cartwidget =  () => {
+  return (
+    <div>
+      <CartFill size={24} /> 0
+    </div>
+  );
+}
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,8 @@
+export const ItemlistContainer = (props) => {
+    return (
+      <div>
+        {props.greeting}
+        <p>Hola! Gracias por visitarnos!</p>
+      </div>
+    );
+  };
